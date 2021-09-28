600279f31d57a71f483eb925b680179d991eaac2
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-28 15:11:17 +0300

    Changing String

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
 import Recursion.RecursiveMethod;
+import TaskString.NewString;
 
 public class Main {
 
     public static void main(String[] args) {
         RecursiveMethod recursiveMethod = new RecursiveMethod();
         recursiveMethod.recursToStack();
+        NewString newString = new NewString();
      }
 }
