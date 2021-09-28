f15754b0e75b4e705bad09e7680ffd7b83af0086
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-28 19:52:06 +0300

    Changing String

--- src/TaskString/NewString.java
+++ src/TaskString/NewString.java
@@ -1,11 +1,15 @@
 package TaskString;
 
+import java.util.Locale;
+
 public class NewString {
 String str = ("onE    oF the biGGest REASONS why    Java is SO poPular is the platform     independence.     \n" +
         "PROGRAMS can run on several different types   OF COMPUTER; as long as THE    computer    has a    \n" +
         "JaVa RUNtime environment (jRE) installed, a jAVA program CAN run oN it.");
 public void changingString(){
-String str =this.str.replaceAll("\\s+"," ");
-System.out.print(str);
+String string =this.str.replaceAll("\\s+"," ");
+//String str2=this.str.toLowerCase(Locale.ROOT);
+//System.out.print(str);
+   System.out.print("\nsecond line :  "+string.toLowerCase(Locale.ROOT) );
     }
 }
