8d2e066e1a516eae33db562169c73792ec9c042f
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-28 19:06:02 +0300

    Changing String

--- src/TaskString/NewString.java
+++ src/TaskString/NewString.java
@@ -1,4 +1,11 @@
 package TaskString;
 
 public class NewString {
+String str = ("onE    oF the biGGest REASONS why    Java is SO poPular is the platform     independence.     \n" +
+        "PROGRAMS can run on several different types   OF COMPUTER; as long as THE    computer    has a    \n" +
+        "JaVa RUNtime environment (jRE) installed, a jAVA program CAN run oN it.");
+public void changingString(){
+String str =this.str.replaceAll("\\s+"," ");
+System.out.print(str);
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,9 @@ import TaskString.NewString;
 public class Main {
 
     public static void main(String[] args) {
-        RecursiveMethod recursiveMethod = new RecursiveMethod();
-        recursiveMethod.recursToStack();
+//        RecursiveMethod recursiveMethod = new RecursiveMethod();
+//        recursiveMethod.recursToStack();
         NewString newString = new NewString();
+        newString.changingString();
      }
 }
