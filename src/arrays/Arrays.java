b7d8aad7303c7cadc26a9b373743c13fd2d0c604
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-29 14:59:15 +0300

    Array Sorting

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -1,4 +1,21 @@
 package arrays;
 
+import java.util.ArrayList;
+
 public class Arrays {
+    public void sortArrat() {
+        int[] numbers = {5, 7, 3, 8, 9, 1};
+        int[] numbers2 = {5, 3, 6, 7, 1, 2};
+        int n = numbers.length;
+        int n2 = numbers2.length;
+        int result[] = new int[n + n2];
+        System.arraycopy(numbers, 0, result, 0, n);
+        System.arraycopy(numbers2, 0, result, n, n2);
+        System.out.println(java.util.Arrays.toString(result));
+        java.util.Arrays.sort(result);
+        for (int i = 0;i< result.length; i++){
+            System.out.print(java.util.Arrays.toString(new int[]{result[i]}));
+        }
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,16 @@ package com.company;
 
 import Recursion.RecursiveMethod;
 import TaskString.NewString;
+import arrays.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 //        RecursiveMethod recursiveMethod = new RecursiveMethod();
 //        recursiveMethod.recursToStack();
-        NewString newString = new NewString();
-        newString.changingString();
+//        NewString newString = new NewString();
+//        newString.changingString();
+        Arrays arrays = new Arrays();
+        arrays.sortArrat();
      }
 }
