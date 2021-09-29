1792d685aa643b87a8b2b69ea34a4786dbd8a404
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-29 15:07:11 +0300

    Array Sorting

--- src/arrays/Arrays.java
+++ src/arrays/Arrays.java
@@ -3,7 +3,7 @@ package arrays;
 import java.util.ArrayList;
 
 public class Arrays {
-    public void sortArrat() {
+    public void sortArray() {
         int[] numbers = {5, 7, 3, 8, 9, 1};
         int[] numbers2 = {5, 3, 6, 7, 1, 2};
         int n = numbers.length;
@@ -11,8 +11,10 @@ public class Arrays {
         int result[] = new int[n + n2];
         System.arraycopy(numbers, 0, result, 0, n);
         System.arraycopy(numbers2, 0, result, n, n2);
-        System.out.println(java.util.Arrays.toString(result));
+        System.out.println("Merged arrays: "+ java.util.Arrays.toString(result));
         java.util.Arrays.sort(result);
+        System.out.println("Result : ");
+
         for (int i = 0;i< result.length; i++){
             System.out.print(java.util.Arrays.toString(new int[]{result[i]}));
         }
