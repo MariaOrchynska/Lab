3944bc574c8a09246494fc3513c8a64b029d5db0
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-30 10:51:47 +0300

    Array 2d Sorting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,12 @@ import arrays.MoreArrays;
 public class Main {
 
     public static void main(String[] args) {
-//        RecursiveMethod recursiveMethod = new RecursiveMethod();
-//        recursiveMethod.recursToStack();
-//        NewString newString = new NewString();
-//        newString.changingString();
-//        Arrays arrays = new Arrays();
-//        arrays.sortArray();
+        RecursiveMethod recursiveMethod = new RecursiveMethod();
+        recursiveMethod.recursToStack();
+        NewString newString = new NewString();
+        newString.changingString();
+        Arrays arrays = new Arrays();
+        arrays.sortArray();
 
     }
 }
